25926380f3e121326ecc6f1ee3b60365caeec522
shseo@digicaps.com <shseo@digicaps.com>
2022-01-20 11:12:05 +0900

    add Strategy Pattern

---
+++ src/io/zoo/animal/lion/behavioral/strategy/VisualStudio.java
@@ -0,0 +1,14 @@
+package io.zoo.animal.lion.behavioral.strategy;
+
+public class VisualStudio extends Ide {
+
+    public VisualStudio(String name) {
+        super(name);
+    }
+
+    @Override
+    public String getName() {
+        return super.getName();
+    }
+
+}
